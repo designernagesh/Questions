9a23dede58da2e7702fdfbba3e29f37487f1e0dc
designernagesh <desginer.nagesh@gmail.com>
2023-06-02 00:54:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './data/data';
+import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 
 function App() {
+  const [ showInfo, setShowInfo ] = useState(null);
+
+  // Alternate Way
+  const toggleHandler = (id) => {
+    if(showInfo === id){
+      return setShowInfo(null)
+     }
+    setShowInfo(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>Questions</h1>
+      <ul>
+        {
+          data.map( question => {
+            return (
+              <li key={ question.id } className='card'>
+                <h3>
+                  {question.title}
+                  <span onClick={ () => toggleHandler(question.id) }>{showInfo === question.id ? <FaAngleUp /> : <FaAngleDown />}</span>
+                </h3>
+                <p className={ showInfo === question.id ? "info show" : "info" }>{ question.info }</p>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
